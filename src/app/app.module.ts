7282e0b7e74bbd8ac5b9e106e053a57919a95cb1
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-06-07 18:55:36 +0400

    generated time and weather components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TimeComponent } from './time/time.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
