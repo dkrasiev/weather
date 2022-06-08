7d168d0086d893edb6878a20192721ac05f25565
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-06-08 20:55:41 +0400

    display time in time component

--- src/app/time/time.component.ts
+++ src/app/time/time.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-time',
   templateUrl: './time.component.html',
-  styleUrls: ['./time.component.css']
+  styleUrls: ['./time.component.css'],
 })
 export class TimeComponent implements OnInit {
+  currentDate: Date = new Date();
+  time: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setInterval(() => {
+      this._updateTime();
+    }, 10);
   }
 
+  private _updateTime() {
+    this.currentDate = new Date();
+
+    const hours = this.currentDate.getHours();
+    const minutes = this.currentDate.getMinutes();
+    const seconds = this.currentDate.getSeconds();
+
+    this.time = hours + ':' + minutes + ':' + seconds;
+  }
 }
