edb67a7f766a253aa9edc0105443f678e64ff379
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-06-08 11:05:53 +0400

    add APIs and tokens to the environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  yandexWeatherApi: 'https://api.weather.yandex.ru/v2/informers',
+  yandexWeatherToken: 'a3c8ab75-528a-4527-bbf2-2e3c114b3ca2',
+  yandexGeocoderApi: 'https://geocode-maps.yandex.ru/1.x',
+  yandexGeocoderToken: '3eb7e3b7-f6df-4f04-8f18-5138e6e70835',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  yandexWeatherApi: 'https://api.weather.yandex.ru/v2/informers',
+  yandexWeatherToken: 'a3c8ab75-528a-4527-bbf2-2e3c114b3ca2',
+  yandexGeocoderApi: 'https://geocode-maps.yandex.ru/1.x',
+  yandexGeocoderToken: '3eb7e3b7-f6df-4f04-8f18-5138e6e70835',
 };
 
 /*
