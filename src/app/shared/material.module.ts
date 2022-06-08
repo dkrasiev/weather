1350fecc789c1654181fd12c6d935f790cc2b8ce
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-06-08 21:03:14 +0400

    add tabs

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
+import { matTabsAnimations, MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatInputModule],
-  exports: [MatButtonModule, MatToolbarModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTabsModule,
+  ],
+  exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatTabsModule],
 })
 export class MaterialModule {}
