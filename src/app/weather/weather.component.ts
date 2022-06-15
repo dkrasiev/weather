e73e3e08aca9ada4da18da7ae21e78022ed0f433
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-06-15 11:18:57 +0400

    delete console.log(e) fix class name in weather component

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -33,8 +33,6 @@ export class WeatherComponent implements OnInit {
         this.autocomplete = [];
       },
       error: (e) => {
-        console.log(e);
-
         this.error = e;
 
         this.isLoading = false;
